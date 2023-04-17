99ae358d6bcb0e73fd4874e7551af4438d8f90e8
Aleksey Efimov <smaylik121@gmail.com>
2023-04-17 21:31:53 +0300

    Initial Commit

---
+++ ColorChangeSwiftUI/ColorChangeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangeSwiftUIApp.swift
+//  ColorChangeSwiftUI
+//
+//  Created by Aleksey Efimov on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
